4afc88c0dc0a11097c54114554353a0de31c4faa
Darabat <aisushin@gmail.com>
2019-05-21 14:38:56 -0300

    Update MPC.py

--- dmbrl/controllers/MPC.py
+++ dmbrl/controllers/MPC.py
@@ -78,7 +78,7 @@ class MPC(Controller):
                         Warning: Can be very memory-intensive
         """
         super().__init__(params)
-        print(params.env.observation_space.shape)
+        print(params.env.observation_space.shape[0])
         self.dO, self.dU = 240, 7
         self.ac_ub, self.ac_lb = params.env.action_space.high, params.env.action_space.low
         self.ac_ub = np.minimum(self.ac_ub, params.get("ac_ub", self.ac_ub))
